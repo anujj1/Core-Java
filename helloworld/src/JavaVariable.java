e737929268d306dc4aff1251fb7a7f319a42ecc6
Anuj <email.jain.anuj@gmail.com>
2021-05-09 13:59:40 +0530

    SecondClassVariableandmedthodintroduction

---
+++ helloworld/src/JavaVariable.java
@@ -0,0 +1,10 @@
+public class JavaVariable {
+    public static void main(String[] args) {
+        int age1 = 10;
+        System.out.println(age1 + 10
+
+
+        );
+    }
+}
+
---
+++ helloworld/src/NewTestClass.java
@@ -0,0 +1,12 @@
+public class NewTestClass {public static void main1(String message){System.out.println(message
+);
+
+}
+
+    public static void main(String[] args) {
+      main1("Anuj");
+      main1("Deepak");
+      main1("Aarav");
+      main1("Akshit");
+    }
+}
